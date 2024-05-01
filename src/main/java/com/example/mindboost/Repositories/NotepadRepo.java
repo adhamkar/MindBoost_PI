9a8f65504f2dd251aa2db2d9ac46ecdfaa92997a
adham07 <adhamkarmadi34@gmail.com>
2024-05-01 18:42:07 +0100

    UserService implementation done

--- src/main/java/com/example/mindboost/Repositories/NotepadRepo.java
+++ src/main/java/com/example/mindboost/Repositories/NotepadRepo.java
@@ -3,10 +3,16 @@ package com.example.mindboost.Repositories;
 import com.example.mindboost.Entities.NotePad;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface NotepadRepo extends JpaRepository<NotePad,Long> {
-    /*@Query(value="SELECT * FROM notepad ORDER BY createddate DESC",nativeQuery = true)
-    List<NotePad> GetAllNotes();*/
+   /* @Query(value="SELECT * FROM NotePad ORDER BY CreatedDate DESC", nativeQuery=true)
+    List<NotePad> getAllNotes();*/
+
+    @Query("select n from NotePad n where n.Title like :kw")
+    List<NotePad> searchByTitle(@Param("kw") String keyword);
+
+    List<String> findNotePadsByTitle();
 }
--- src/main/java/com/example/mindboost/Repositories/PostRepo.java
+++ src/main/java/com/example/mindboost/Repositories/PostRepo.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PostRepo extends JpaRepository<Post,Long> {
-    /*@Query(value="SELECT * FROM post ORDER BY createddate DESC",nativeQuery = true)
-    List<Post> GetAllPosts();*/
+    @Query(value="SELECT * FROM post ORDER BY CreatedDate DESC",nativeQuery = true)
+    List<Post> getAllPosts();
+    //List<Post> getAllByCreatedDateOrderByCreatedDate();
 }
