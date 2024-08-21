a0c2dee70534322f96731827529dbf2cec53af81
adham07 <120655777+adhamkar@users.noreply.github.com>
2024-08-22 00:35:50 +0100

    Update AccountService.java

--- src/main/java/com/example/mindboost/Security/Service/AccountService.java
+++ src/main/java/com/example/mindboost/Security/Service/AccountService.java
@@ -4,7 +4,7 @@ import com.example.mindboost.Security.entities.AppRole;
 import com.example.mindboost.Security.entities.AppUser;
 
 public interface AccountService {
-    AppUser AddUser(String userName, String password, String email,String confirmedPassword);
+    AppUser AddUser(String userName, String password, String email,String confirmedPassword); // i can use it as sign up
     AppUser LoadUserByUserName(String userName);
     void AddRoleToUser(String userName, String roleName);
     AppRole AddRole(String role);
